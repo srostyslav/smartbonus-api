25461bd36f906eb4acf5886f737d76d96b7dfb0f
Ros <rostyslav.rigroup@gmail.com>
2023-10-04 18:42:52 +0300

    add by_store

--- receipt_discount.go
+++ receipt_discount.go
@@ -8,6 +8,7 @@ type NomenclatureItem struct {
 	Id       string  `json:"nomenclature_id"` // your product identifier
 	Quantity float64 `json:"amount"`          // quantity or product
 	Price    float64 `json:"unit_price"`      // price of product
+	ByStore  bool    `json:"by_store"`
 }
 
 // Body for receipt discount method
